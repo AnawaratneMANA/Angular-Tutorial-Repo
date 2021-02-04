e780f8fe96064040a9e837475e28c9a621899604
Nirmith Akash <IT19086408@my.sliit.lk>
2021-02-04 16:32:08 +0530

    Udemy Free Course - Angular 2 (Custom events and Emitter Events).

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {COURSES} from '../db-data';
+import {Course} from './model/course';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,13 @@ export class AppComponent {
   object1 = COURSES[0];
   object2 = COURSES[1];
   object3 = COURSES[2];
+
+  // Create method onClassClicked()
+  onCardClicked(course: Course) {
+    console.log('App component - click event bubbled', course);
+  }
+
+  onCourseSelected(course: Course) {
+    console.log('App component - Custom event bubbled..', course);
+  }
 }
--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,19 +1,31 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Course } from '../model/course'
+import {Component, Input, OnInit, Output} from '@angular/core';
+import { Course } from '../model/course';
+import {EventEmitter} from '@angular/core';
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'course-card',
   templateUrl: './course-card.component.html',
   styleUrls: ['./course-card.component.css']
 })
 export class CourseCardComponent implements OnInit {
 
-  constructor() { }
-
   @Input()
   c: Course;
 
+  @Output()
+  courseSelected = new EventEmitter<Course>();
+
+  constructor() { }
+
   ngOnInit(): void {
+
+  }
+
+  onCourseViewed() {
+    console.log('card component - button clicked');
+    this.courseSelected.emit(this.c);
+    // Emitter is sending the c object.
   }
 
 }
